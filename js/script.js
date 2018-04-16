a910b4ed5045c9ef53dc699c118ea173917270d5
liuoioi <liuoioi1017@gmail.com>
2018-04-16 17:10:55 +0800

    Test10

--- js/script.js
+++ js/script.js
@@ -51,14 +51,12 @@ $(function () {
   // initialize skrollr if the window width is large enough
   if ($(window).width() > 1025) {
     skrollr.init("#bg1");
-	  new WOW().init();
   }
 
   // disable skrollr if the window is resized below 768px wide
   $(window).on('resize', function () {
     if ($(window).width() <= 1025) {
       skrollr.init().destroy(); // skrollr.init() returns the singleton created above
-		WOW().init().destroy();
     }
   });
 });
